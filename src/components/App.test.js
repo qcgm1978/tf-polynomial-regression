19d83832a0cd9438bde9b07deb631f3ae5d58521
zhanghl-k <zhanghl-k@glodon.com>
2018-05-08 18:36:10 +0800

    test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it(`https://en.wikipedia.org/wiki/Bernoulli_process`, () => {
+  let num = parseInt('101010', 2);
+  expect(num).toBe(42)
+  num = parseInt('10011011', 2)
+  expect(num).toBe(155)
+  num = Number(0o10011011.toString())
+  expect(num).toBe(2101769)
 });
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
